b4f7b111628edc76276aef776a823bc5a338ba00
John MacGregor <jonathan.macgregor@accenture.com>
2019-03-19 09:41:07 -0400

    Almost-working angular app

--- AngularExperiments/AngularExperiments.Web/wwwroot/systemjs.config.js
+++ AngularExperiments/AngularExperiments.Web/wwwroot/systemjs.config.js
@@ -1,26 +1,27 @@
-(function (global) {
+(function () {
     System.config({
         paths: {
-            'npm:': 'node_modules/'
+            'npm:': '/node_modules/'
         },
         map: {
-            'app': 'app',
+            'app': '/src/app',
             '@angular/common': 'npm:@angular/common/bundles/common.umd.js',
             '@angular/compiler': 'npm:@angular/compiler/bundles/compiler.umd.js',
             '@angular/core': 'npm:@angular/core/bundles/core.umd.js',
             '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
             '@angular/platform-browser': 'npm:@angular/platform-browser/bundles/platform-browser.umd.js',
             '@angular/platform-browser-dynamic': 'npm@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
-            'rxjs': 'npm:rxjs'
+            'rxjs': 'npm:rxjs',
+            'loader': '/src/systemjs-angular-loader.js'
         },
         packages: {
             app: {
-                main: './main.js',
-                defaultExtension: 'js'
+                main: 'main.ts',
+                defaultExtension: 'ts'
             },
             rxjs: {
-                defaultExtension: 'js'
+                defaultExtension: 'ts'
             }
         }
     });
-}) (this);
+});
